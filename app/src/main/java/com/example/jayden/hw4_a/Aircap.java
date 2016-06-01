318fc3edaf4e116eba2d5bea02bebe5e30012ff8
Jiwon <jiwon2772@gmail.com>
2016-06-02 05:20:31 +0900

    first commit

---
+++ app/src/main/java/com/example/jayden/hw4_a/Aircap.java
@@ -0,0 +1,13 @@
+package com.example.jayden.hw4_a;
+
+/**
+ * Created by Jayden on 2016-05-25.
+ */
+public class Aircap
+{
+    public boolean state;
+
+    Aircap() {
+        state = true;
+    }
+}
